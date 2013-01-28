d679e8e40112da7825bee8500ca220a4b178250b
Joshua T Kalis <kalisjoshua@gmail.com>
2013-01-28 16:20:43 -0500

    better comments to explain what is happening and why

--- jquery-plugin/js/versions/plugin.v1.jquery.js
+++ jquery-plugin/js/versions/plugin.v1.jquery.js
@@ -1,28 +1,32 @@
 (function ($) {
-  $.fn.ISMs = function (folder, images, speed) {
+  $.fn.ISMs = function (folder, images, speed) {                // define jQuery plugin on prototype (fn, a shortcut for prototype)
 
-    return this
-      .each(function (indx, item) {
-        var img     = $("<img />"),
-            imgList = images.slice(0);
+    return this                                                 // return the jQuery collection to not break chaining
+      .each(function (indx, item) {                             // create function scope for each plugin on a page
+        var img     = $("<img />"),                             // reference to DOM object that will be used for displaying the image
+            imgList = images.slice(0);                          // copy of images list so that each jQuery object will have its own
 
-        function animate (now) {
-          setTimeout(nextImage, (now ? 1 : speed));
+        function nextImage (now) {
+          img.attr("src", folder + "/" + imgList[0]);           // swap out the the currently displayed image for the next one
+          imgList = imgList.slice(1).concat(imgList[0]);        // move the displayed image to the end of the list
+          setTimeout(nextImage, (now ? 1 : speed));             // continue/start the animation and run infinitely
         }
 
-        function nextImage () {
-          img.attr("src", folder + "/" + imgList[0]);
-          imgList = imgList.slice(1).concat(imgList[0]);
-          animate();
-        }
+        nextImage(true);                                        // imidiately display the first image and delay all following
 
-        animate(true);
-
-        $(item).html(img);
+        $(item).html(img);                                      // add the image to the DOM so users can see it
       });
   };
 }(jQuery));
 
+
+
+
+
+
+
+// This should be in app.js but it is easier to have it in here for demo
+// purposes, since the config will be changing over time.
 $.fn.ready(function () {
   $(".ISMs.fast").ISMs(folder, images, 400);
   $(".ISMs.slow").ISMs(folder, images, 4000);
---
+++ jquery-plugin/js/versions/plugin.v2.jquery.js
@@ -0,0 +1,51 @@
+(function ($) {
+  $.fn.ISMs = function (folder, images, speed) {                // define jQuery plugin on prototype (fn, a shortcut for prototype)
+
+    return this                                                 // return the jQuery collection to not break chaining
+      .each(function (indx, item) {                             // create function scope for each plugin on a page
+        var hovering  = false,
+            img     = $("<img />"),                             // reference to DOM object that will be used for displaying the image
+            imgList = images.slice(0),                          // copy of images list so that each jQuery object will have its own
+            pending;
+
+        function animate (now) {                                // factored out the animation logic from the 'nextImage' logic
+          clearTimeout(pending);                                // clear timeouts if there is one since this method will be used in differenct contexts
+
+          if (!hovering) {                                      // only if the user is not currently hovering should the animation continue
+            pending = setTimeout(function () {                  // create a composite function to do the actions of the animation
+              nextImage();                                      // let the functio do its job
+              animate();                                        // continue the animation
+            }, (now ? 1 : speed));
+          }
+        }
+
+        function nextImage (now) {
+          img.attr("src", folder + "/" + imgList[0]);           // swap out the the currently displayed image for the next one
+          imgList = imgList.slice(1).concat(imgList[0]);        // move the displayed image to the end of the list
+          // setTimeout(nextImage, (now ? 1 : speed));          // removed to better adhere to what the function is actually for
+        }
+
+        animate(true);                                          // imidiately display the first image and delay all following
+
+        $(item)
+          .html(img)                                            // add the image to the DOM so users can see it
+          .on("mouseover mouseout", function (event) {
+            hovering = !hovering;                               // toggle the state of hovering
+            animate();                                          // either clear the next animation or start a new one based on the event
+          });
+      });
+  };
+}(jQuery));
+
+
+
+
+
+
+
+// This should be in app.js but it is easier to have it in here for demo
+// purposes, since the config will be changing over time.
+$.fn.ready(function () {
+  $(".ISMs.fast").ISMs(folder, images, 400);
+  $(".ISMs.slow").ISMs(folder, images, 4000);
+});
