6f10cf1c5ac2b0523b27907818864e0c67788557
Joshua T Kalis <kalisjoshua@gmail.com>
2013-02-02 21:36:35 -0500

    removing unnecessary code

--- refactoring/rental/test.js
+++ refactoring/rental/test.js
@@ -11,23 +11,10 @@ eval(fs.readFileSync(file, "UTF-8"));
 
 var me = new Customer("My Self");
 
-// var movies = [
-//       /* 0 */ new Movie("The Karate Kid", 0),
-//       /* 1 */ new Movie("The Karate Kid II", 0),
-//       /* 2 */ new Movie("Cujo", 0),
-//       /* 3 */ new Movie("Boyz N the Hood", 0),
-//       /* 4 */ new Movie("Balls of Fury", 1),
-//       /* 5 */ new Movie("Waiting", 1),
-//       /* 6 */ new Movie("Bolt", 2),
-//       /* 7 */ new Movie("The Adjustment Bureau", 1),
-//       /* 8 */ new Movie("She's the Man", 1),
-//       /* 9 */ new Movie("Hercules", 2)
-//     ];
-
 me.addRental(new Rental(new Movie("The Karate Kid",        0), 1));
 me.addRental(new Rental(new Movie("The Karate Kid II",     0), 3));
-me.addRental(new Rental(new Movie("Bolt",                  1), 2));
-me.addRental(new Rental(new Movie("Hercules",              2), 5));
+me.addRental(new Rental(new Movie("Bolt",                  2), 5));
+me.addRental(new Rental(new Movie("Tangled",               1), 2));
 me.addRental(new Rental(new Movie("The Adjustment Bureau", 1), 1));
 
 console.log(me.statement());
